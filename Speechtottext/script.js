1184f9573bd74765ba906702745f0f8f175c5f4f
Faiiss <33278@ma-web.nl>
2022-06-08 12:19:00 +0200

    Update

---
+++ Speechtottext/script.js
@@ -0,0 +1,5 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const message = document.getElementById("message");
+const output = document.getElementById("result");
+const image1 = document.getElementById("image1");
